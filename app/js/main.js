c67beb7a4460f0a0bb65fb06a1d7f2c9858676a4
syirus <syirus99@gmail.com>
2020-04-09 15:52:15 +0300

    added product-slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function () {
 
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true
+
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToScroll: 4,
+        slidesToShow: 4
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 
